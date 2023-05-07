1c010d7929d3b2ddc54b1fe24c65bf3ee3d987ab
boydstacken <124229670+boydstacken@users.noreply.github.com>
2023-05-07 17:07:07 -0500

    Second commit, adjusted code and added inquirer prompt to create the array of questions for the user.

--- Develop/index.js
+++ Develop/index.js
@@ -1,24 +1,45 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require ('inquirer')
+const fs = require ('fs')
 
 // TODO: Create an array of questions for user input
-const questions = ["What is your first and last name?", 
-"What is your README title?", 
-"Please include a description of your README", 
-"What are the installation instructions?", 
-"Please describe usage",
-"Who were contributors for the proect?"];
-
-const fs = require ('fs')
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName,data,(err) => {
-    if (err) throw err;
-    console.log(err) : console.log('Success!');
-    })
-}
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your first and last name?',
+      name: 'name',
+    },
+    {
+      type: 'input',
+      message: 'What is the title of your README?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Please include a description of your README',
+      name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions?',
+        name: 'installation instructions',
+    },
+    {
+        type: 'input',
+        message: 'Please describe the usage for this application ',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Who were contributors for the project?',
+        name: 'contributors',
+    },
+  ])
+  .then((response) =>{
+  const filename = `${response.name.toLowerCase().split(' ').join('')}.json`;
+  // TODO: Create a function to write README file
+  fs.writeFile(filename,JSON.stringify(response), err=> {
+      err?console.log(err):console.log("sucess")
+  })
+})
